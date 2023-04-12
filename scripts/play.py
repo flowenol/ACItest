acf76da0153b865b59e2fd63a15b1ae37b3c3218
Piotr Jaczewski <flovenol@gmail.com>
2023-04-12 23:49:31 +0200

    volume level request added

--- scripts/play.py
+++ scripts/play.py
@@ -4,11 +4,17 @@ import sys
 import serial
 import select
 import signal
+import threading
+import types
 
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
 
-played_loops: int  = 0
+commands = types.SimpleNamespace()
+commands.START_PLAYBACK = b'S'
+commands.PLAYBACK_FINISHED = b'S'
+commands.VOLUME = b'V'
+
 
 def get_serial(port: str) -> serial.Serial:
     s = serial.Serial()
@@ -17,31 +23,47 @@ def get_serial(port: str) -> serial.Serial:
     s.stopbits = serial.STOPBITS_ONE
     s.bytesize = serial.EIGHTBITS
     s.port = port
-    s.timeout = None
+    s.timeout = 1
     s.write_timeout = None
     return s
 
 
-def data_ready_on_port(port: serial.Serial):
-    return bool(select.select([port.fileno()], [], [], 0)[0])
+def data_ready_on_port():
+    return bool(select.select([port.fileno()], [], [], 1)[0])
 
-
-def wait_for_start_signal(port: str):
-    with get_serial(port) as s:
-        print('Waiting for a signal to start... ', file=sys.stderr, end='', flush=True)
-        while not data_ready_on_port(s):
-            pass
-        print('OK', file=sys.stderr)
-
-        s.timeout = 1
-        s.read(size = 1)
+def wait_for_data():
+    while not signal_thread_stop.is_set():
+        if not data_ready_on_port():
+            continue
         
+        command = port.read(size = 1)
+        match command:
+            case commands.START_PLAYBACK:
+                with playback_signal_sent:
+                    playback_signal_sent.notify_all()
+            case commands.VOLUME:
+                signal_volume_level()
+            case other:
+                pass
+    
+    with signal_thread_stopped:
+        signal_thread_stopped.notify_all()
 
-def signal_playback_finished(port: str):
-    with get_serial(port) as s:
-        s.write(bytes(ord('S')))
-        s.flush()
-        
+
+def signal_playback_finished():
+    port.write(commands.PLAYBACK_FINISHED)
+    port.flush()
+
+
+def signal_volume_level():
+    def volume_level_to_bcd():
+        global volume_level
+        assert volume_level >= 0 and volume_level <= 100
+        return (((int(volume_level / 10) & 0x0f) << 4) | (int(volume_level % 10) & 0x0f)).to_bytes(1, 'little')
+            
+    port.write(volume_level_to_bcd())
+    port.flush()        
+
 
 def playback(recording: str, volume: float = 1.0):
     pygame.mixer.init()
@@ -50,6 +72,15 @@ def playback(recording: str, volume: float = 1.0):
     pygame.mixer.music.play()
     while pygame.mixer.music.get_busy():
         pass
+
+
+played_loops: int  = 0
+port = None
+playback_signal_sent = threading.Condition()
+signal_thread = threading.Thread(target=wait_for_data, daemon=True)
+signal_thread_stop = threading.Event()
+signal_thread_stopped = threading.Condition()
+volume_level: int = 100
 
 """
 This utility plays the ACI program wav file. The utility works in an infinite loop:
@@ -79,13 +110,27 @@ if __name__ == "__main__":
 
     def print_played_loops_and_exit(status: int):
         print(f'\nPlayed {played_loops} times', file=sys.stderr)
+        signal_thread_stop.set()
+        
+        with signal_thread_stopped:
+            signal_thread_stopped.wait(1)
+
         exit(status)
 
     # terminate silently on SIGINT
     signal.signal(signal.SIGINT, lambda s, fr: print_played_loops_and_exit(1))
 
-    while True:
-        wait_for_start_signal(args.device)
-        playback(args.file[0], args.volume / 100.0)
-        signal_playback_finished(args.device)
-        played_loops += 1
+    volume_level = args.volume
+
+    with get_serial(args.device) as port:
+        
+        signal_thread.start()
+
+        while True:
+            print('Waiting for a signal to start... ', file=sys.stderr, end='', flush=True)
+            with playback_signal_sent:
+                playback_signal_sent.wait()
+            print('OK', file=sys.stderr)
+            playback(args.file[0], volume_level / 100.0)
+            signal_playback_finished()
+            played_loops += 1
